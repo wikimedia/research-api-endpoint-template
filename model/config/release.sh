2238704e5964714dda6d22e88c7a895f8eba18b9
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2020-07-27 12:39:05 -0400

    merge updates from master

--- model/config/release.sh
+++ model/config/release.sh
@@ -4,9 +4,9 @@
 APP_LBL='api-endpoint'  # descriptive label for endpoint-related directories
 REPO_LBL='topicmodel'  # directory where repo code will go
 GIT_CLONE_HTTPS='https://github.com/geohci/research-api-endpoint-template.git'  # for `git clone`
-ETC_PATH='/etc/${APP_LBL}'  # app config info, scripts, ML models, etc.
-SRV_PATH='/srv/${APP_LBL}'  # application resources for serving endpoint
-TMP_PATH='/tmp/${APP_LBL}'  # store temporary files created as part of setting up app (cleared with every update)
+ETC_PATH="/etc/${APP_LBL}"  # app config info, scripts, ML models, etc.
+SRV_PATH="/srv/${APP_LBL}"  # application resources for serving endpoint
+TMP_PATH="/tmp/${APP_LBL}"  # store temporary files created as part of setting up app (cleared with every update)
 LIB_PATH="/var/lib/${APP_LBL}"  # where virtualenv will sit
 
 # clean up old versions
@@ -25,7 +25,22 @@ echo "Installing repositories..."
 pip install wheel
 pip install -r ${TMP_PATH}/${REPO_LBL}/requirements.txt
 
-# uncomment if config changes:
-# cp ${TMP_PATH}/${REPO_LBL}/config/* ${ETC_PATH}
+# update config / code -- if only changing Python and not nginx/uwsgi code, then much of this can be commented out
+echo "Copying configuration files..."
+cp ${TMP_PATH}/${REPO_LBL}/model/config/* ${ETC_PATH}
+# TODO: fix this to be more elegant (one directory or not necessary because run as package)
+cp ${TMP_PATH}/${REPO_LBL}/model/wsgi.py ${ETC_PATH}
+cp ${TMP_PATH}/${REPO_LBL}/model/flask_config.yaml ${ETC_PATH}
+cp ${ETC_PATH}/model.nginx /etc/nginx/sites-available/model
+if [[ -f "/etc/nginx/sites-enabled/model" ]]; then
+    unlink /etc/nginx/sites-enabled/model
+fi
+ln -s /etc/nginx/sites-available/model /etc/nginx/sites-enabled/
+cp ${ETC_PATH}/model.service /etc/systemd/system/
 
-systemctl restart model.service
+echo "Enabling and starting services..."
+systemctl enable model.service  # uwsgi starts when server starts up
+systemctl daemon-reload  # refresh state
+
+systemctl restart model.service  # start up uwsgi
+systemctl restart nginx  # start up nginx
