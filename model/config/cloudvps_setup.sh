31861c3929b919141962a3847575550a48111128
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2021-04-08 16:39:38 -0400

    test basic setup:

--- model/config/cloudvps_setup.sh
+++ model/config/cloudvps_setup.sh
@@ -3,9 +3,9 @@
 
 # these can be changed but most other variables should be left alone
 APP_LBL='api-endpoint'  # descriptive label for endpoint-related directories
-REPO_LBL='topicmodel'  # directory where repo code will go
-GIT_CLONE_HTTPS='https://github.com/geohci/research-api-endpoint-template.git'  # for `git clone`
-MODEL_WGET='https://ndownloader.figshare.com/files/<file-number>'  # model binary -- ndownloader.figshare is a good host
+REPO_LBL='turnilo'  # directory where repo code will go
+GIT_CLONE_HTTPS='https://github.com/wikimedia/research-api-endpoint-template.git'  # for `git clone`
+DATA_WGET='https://ndownloader.figshare.com/files/<file-number>'  # model binary -- ndownloader.figshare is a good host
 
 ETC_PATH="/etc/${APP_LBL}"  # app config info, scripts, ML models, etc.
 SRV_PATH="/srv/${APP_LBL}"  # application resources for serving endpoint
@@ -15,15 +15,13 @@ LIB_PATH="/var/lib/${APP_LBL}"  # where virtualenv will sit
 
 echo "Updating the system..."
 apt-get update
-apt-get install -y build-essential  # gcc (c++ compiler) necessary for fasttext
+apt-get install -y build-essential  # basic tools that are needed for some node packages
 apt-get install -y nginx  # handles incoming requests, load balances, and passes to uWSGI to be fulfilled
-apt-get install -y python3-pip  # install dependencies
-apt-get install -y python3-wheel  # make sure dependencies install correctly even when missing wheels
-apt-get install -y python3-venv  # for building virtualenv
-apt-get install -y python3-dev  # necessary for fasttext
-apt-get install -y uwsgi
-apt-get install -y uwsgi-plugin-python3
-# potentially add: apt-get install -y git python3 libpython3.7 python3-setuptools
+curl -fsSL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh  # bring in nodejs source
+bash nodesource_setup.sh  # add nodejs v14 source to apt-get
+apt-get install -y nodejs  # install nodejs v14
+apt-get install -y npm  # install npm
+npm install -g turnilo  # install turnilo
 
 echo "Setting up paths..."
 rm -rf ${TMP_PATH}
@@ -34,43 +32,14 @@ mkdir -p ${ETC_PATH}/resources
 mkdir -p ${LOG_PATH}
 mkdir -p ${LIB_PATH}
 
-echo "Setting up virtualenv..."
-python3 -m venv ${LIB_PATH}/p3env
-source ${LIB_PATH}/p3env/bin/activate
-
-echo "Cloning repositories..."
-# NOTE: a more stable install would involve building wheels on an identical instance and then the following:
-# NOTE: see (https://gerrit.wikimedia.org/g/research/recommendation-api/wheels/+/refs/heads/master) for an example.
-# git clone https://gerrit.wikimedia.org/r/research/recommendation-api/wheels ${TMP_PATH}/wheels
-# echo "Making wheel files..."
-# cd ${TMP_PATH}/wheels
-# rm -rf wheels/*.whl
-# make
-# git clone ${GIT_CLONE_HTTPS} ${TMP_PATH}/${REPO_LBL}
-# echo "Installing repositories..."
-# pip3 install --no-deps ${TMP_PATH}/wheels/wheels/*.whl
-# pip3 install --no-deps ${TMP_PATH}/recommendation-api
-
 # The simpler process is to just install dependencies per a requirements.txt file
 # With updates, however, the packages could change, leading to unexpected behavior or errors
-git clone ${GIT_CLONE_HTTPS} ${TMP_PATH}/${REPO_LBL}
-
-echo "Installing repositories..."
-pip install wheel
-pip install -r ${TMP_PATH}/${REPO_LBL}/requirements.txt
-
-# If UI included, consider the following for managing JS dependencies:
-# echo "Installing front-end resources..."
-# mkdir -p ${SRV_PATH}/resources
-# cd ${TMP_PATH}
-# npm install bower
-# cd ${SRV_PATH}/resources
-# ${TMP_PATH}/node_modules/bower/bin/bower install --allow-root ${TMP_PATH}/recommendation-api/recommendation/web/static/bower.json
+git clone --branch turnilo ${GIT_CLONE_HTTPS} ${TMP_PATH}/${REPO_LBL}
 
 echo "Downloading model, hang on..."
 #cd ${TMP_PATH}
-#wget -O model.bin ${MODEL_WGET}
-#mv model.bin ${ETC_PATH}/resources
+#wget -O data.json ${DATA_WGET}
+#mv data.json ${ETC_PATH}/resources
 
 echo "Setting up ownership..."  # makes www-data (how nginx is run) owner + group for all data etc.
 chown -R www-data:www-data ${ETC_PATH}
--- model/config/release.sh
+++ model/config/release.sh
@@ -2,8 +2,10 @@
 # update API endpoint with new model, code, etc.
 
 APP_LBL='api-endpoint'  # descriptive label for endpoint-related directories
-REPO_LBL='topicmodel'  # directory where repo code will go
-GIT_CLONE_HTTPS='https://github.com/geohci/research-api-endpoint-template.git'  # for `git clone`
+REPO_LBL='turnilo'  # directory where repo code will go
+GIT_CLONE_HTTPS='https://github.com/wikimedia/research-api-endpoint-template.git'  # for `git clone`
+DATA_WGET='https://ndownloader.figshare.com/files/<file-number>'  # model binary -- ndownloader.figshare is a good host
+
 ETC_PATH="/etc/${APP_LBL}"  # app config info, scripts, ML models, etc.
 SRV_PATH="/srv/${APP_LBL}"  # application resources for serving endpoint
 TMP_PATH="/tmp/${APP_LBL}"  # store temporary files created as part of setting up app (cleared with every update)
@@ -13,24 +15,18 @@ LIB_PATH="/var/lib/${APP_LBL}"  # where virtualenv will sit
 rm -rf ${TMP_PATH}
 mkdir -p ${TMP_PATH}
 
-git clone ${GIT_CLONE_HTTPS} ${TMP_PATH}/${REPO_LBL}
+git clone --branch turnilo ${GIT_CLONE_HTTPS} ${TMP_PATH}/${REPO_LBL}
 
-# reinstall virtualenv
-rm -rf ${LIB_PATH}/p3env
-echo "Setting up virtualenv..."
-python3 -m venv ${LIB_PATH}/p3env
-source ${LIB_PATH}/p3env/bin/activate
-
-echo "Installing repositories..."
-pip install wheel
-pip install -r ${TMP_PATH}/${REPO_LBL}/requirements.txt
+echo "Downloading model, hang on..."
+#cd ${TMP_PATH}
+#wget -O data.json ${DATA_WGET}
+#mv data.json ${ETC_PATH}/resources
+#chown -R www-data:www-data ${ETC_PATH}
 
 # update config / code -- if only changing Python and not nginx/uwsgi code, then much of this can be commented out
 echo "Copying configuration files..."
 cp ${TMP_PATH}/${REPO_LBL}/model/config/* ${ETC_PATH}
 # TODO: fix this to be more elegant (one directory or not necessary because run as package)
-cp ${TMP_PATH}/${REPO_LBL}/model/wsgi.py ${ETC_PATH}
-cp ${TMP_PATH}/${REPO_LBL}/model/flask_config.yaml ${ETC_PATH}
 cp ${ETC_PATH}/model.nginx /etc/nginx/sites-available/model
 if [[ -f "/etc/nginx/sites-enabled/model" ]]; then
     unlink /etc/nginx/sites-enabled/model
