1e5131da21a57907d651e57d0d0977b670399dc9
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2022-03-15 15:03:04 -0400

    try two

--- model/config/cloudvps_setup.sh
+++ model/config/cloudvps_setup.sh
@@ -4,22 +4,29 @@
 # these can be changed but most other variables should be left alone
 APP_LBL='api-endpoint'  # descriptive label for endpoint-related directories
 REPO_LBL='languagetool'  # directory where repo code will go
+
+# code
 GIT_CLONE_HTTPS='https://github.com/geohci/research-api-endpoint-template.git'  # for `git clone`
-# model binary / data -- ndownloader.figshare is a good host
-# alternatives include analytics -- e.g., https://analytics.wikimedia.org/published/datasets/one-off/isaacj/...
-# for more details, see: https://wikitech.wikimedia.org/wiki/Analytics/Web_publication
-LANGTOOLS_ZIP='https://languagetool.org/download/LanguageTool-stable.zip'
 GIT_BRANCH='language-tool'
+
+# langtools application
+LANGTOOLS_ZIP='https://languagetool.org/download/LanguageTool-stable.zip'
+
+# java installation
+JAVA8_WGET_URL="https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245797_df5ad55fdd604472a86a45a217032c7d"
+JAVA8_TARNAME="jre-8u321-linux-x64_2.tar.gz"
+JAVA8_DIRNAME="jre1.8.0_321"
+JAVA_EXPECTED_SHASUM='b6d6e505cc1d48c670d69edfd5beae5717472512'
 
 # derived paths
 ETC_PATH="/etc/${APP_LBL}"  # app config info, scripts, ML models, etc.
+JAVA_PATH="/var/lib/jvm"
 TMP_PATH="/tmp/${APP_LBL}"  # store temporary files created as part of setting up app (cleared with every update)
 
 echo "Updating the system..."
 apt-get update
 apt-get install -y build-essential  # gcc (c++ compiler) necessary for fasttext
 apt-get install -y nginx  # handles incoming requests, load balances, and passes to uWSGI to be fulfilled
-apt-get install openjdk-8-jre
 # potentially add: apt-get install -y git python3 libpython3.7 python3-setuptools
 
 echo "Setting up paths..."
@@ -35,6 +42,12 @@ echo "Downloading language tools, hang on..."
 cd ${TMP_PATH}
 wget -O LanguageTool-stable.zip ${LANGTOOLS_ZIP}
 unzip LanguageTool-stable.zip -d ${ETC_PATH}/lt
+
+echo "Downloading Java8, hang on..."
+wget -O "${JAVA8_TARNAME}" "${JAVA8_WGET_URL}"
+echo "${JAVA_EXPECTED_SHASUM} ${JAVA8_TARNAME}" | shasum -c - || exit 1
+tar zxvf "${JAVA8_TARNAME}" -C "${JAVA_PATH}"
+sudo update-alternatives --install "/usr/bin/java" "java" "${JAVA_PATH}/${JAVA8_DIRNAME}/bin/java" 1
 
 echo "Setting up ownership..."  # makes www-data (how nginx is run) owner + group for all data etc.
 chown -R www-data:www-data ${ETC_PATH}
--- model/config/release.sh
+++ model/config/release.sh
@@ -3,14 +3,15 @@
 
 # these can be changed but most other variables should be left alone
 APP_LBL='api-endpoint'  # descriptive label for endpoint-related directories
-REPO_LBL='topicmodel'  # directory where repo code will go
+REPO_LBL='languagetool'  # directory where repo code will go
+
+# code
 GIT_CLONE_HTTPS='https://github.com/geohci/research-api-endpoint-template.git'  # for `git clone`
-GIT_BRANCH='master'
+GIT_BRANCH='language-tool'
 
 # derived paths
 ETC_PATH="/etc/${APP_LBL}"  # app config info, scripts, ML models, etc.
 TMP_PATH="/tmp/${APP_LBL}"  # store temporary files created as part of setting up app (cleared with every update)
-LIB_PATH="/var/lib/${APP_LBL}"  # where virtualenv will sit
 
 # clean up old versions
 rm -rf ${TMP_PATH}
@@ -18,21 +19,10 @@ mkdir -p ${TMP_PATH}
 
 git clone --branch ${GIT_BRANCH} ${GIT_CLONE_HTTPS} ${TMP_PATH}/${REPO_LBL}
 
-# reinstall virtualenv
-rm -rf ${LIB_PATH}/p3env
-echo "Setting up virtualenv..."
-python3 -m venv ${LIB_PATH}/p3env
-source ${LIB_PATH}/p3env/bin/activate
-
-echo "Installing repositories..."
-pip install wheel
-pip install -r ${TMP_PATH}/${REPO_LBL}/requirements.txt
-
 # update config / code -- if only changing Python and not nginx/uwsgi code, then much of this can be commented out
 echo "Copying configuration files..."
-cp ${TMP_PATH}/${REPO_LBL}/model/config/* ${ETC_PATH}
-cp ${TMP_PATH}/${REPO_LBL}/model/wsgi.py ${ETC_PATH}
-cp ${TMP_PATH}/${REPO_LBL}/model/flask_config.yaml ${ETC_PATH}
+cp ${TMP_PATH}/${REPO_LBL}/model/config/model.nginx ${ETC_PATH}
+cp ${TMP_PATH}/${REPO_LBL}/model/config/model.service ${ETC_PATH}
 cp ${ETC_PATH}/model.nginx /etc/nginx/sites-available/model
 if [[ -f "/etc/nginx/sites-enabled/model" ]]; then
     unlink /etc/nginx/sites-enabled/model
