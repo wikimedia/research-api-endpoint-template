64274b350526d1ce46b31407f45bbfc2ad1e7a6d
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2022-06-06 21:45:07 -0400

    first attempt druid backed turnilo

--- model/config/cloudvps_setup.sh
+++ model/config/cloudvps_setup.sh
@@ -3,57 +3,68 @@
 
 # folder labels
 APP_LBL='api-endpoint'  # descriptive label for endpoint-related directories
-REPO_LBL='languagetool'  # directory where repo code will go
+REPO_LBL='turnilo'  # directory where repo code will go
 
 # where this code lives / config for running the server
 GIT_CLONE_HTTPS='https://github.com/geohci/research-api-endpoint-template.git'  # for `git clone`
-GIT_BRANCH='language-tool'
+GIT_BRANCH='turnilo-druid'
+DATA_WGET='https://analytics.wikimedia.org/published/datasets/one-off/isaacj/referrals/data.tsv'  # data for Turnilo
 
-# langtools application -- this is downloaded and has a Java jar that runs the server (see model.service)
-LANGTOOLS_ZIP='https://languagetool.org/download/LanguageTool-stable.zip'
+# Druid database -- this is downloaded and runs the backend database for the search data and turnilo
+DRUID_APP='https://dlcdn.apache.org/druid/0.22.1/apache-druid-0.22.1-bin.tar.gz'
+DRUID_TARNAME='apache-druid-0.22.1-bin.tar.gz'
+DRUID_EXPECTED_SHASUM='716b83e07a76b5c9e0e26dd49028ca088bde81befb070989b41e71f0e8082d11a26601f4ac1e646bf099a4bc7420bdfeb9f7450d6da53d2a6de301e08c3cab0d'
+DRUID_DIRNAME="apache-druid-0.22.1"
+DRUID_PATH='/var/lib/druid'
 
 # java8 installation -- unfortunately not supported via standard apt-get just yet
 JAVA8_WGET_URL="https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245797_df5ad55fdd604472a86a45a217032c7d"
 JAVA8_TARNAME="jre-8u321-linux-x64.tar.gz"
 JAVA8_DIRNAME="jre1.8.0_321"
 JAVA_EXPECTED_SHASUM='b6d6e505cc1d48c670d69edfd5beae5717472512'
+JAVA_PATH="/var/lib/jvm"
 
 # derived paths
 ETC_PATH="/etc/${APP_LBL}"  # app config info, scripts, ML models, etc.
-JAVA_PATH="/var/lib/jvm"
 TMP_PATH="/tmp/${APP_LBL}"  # store temporary files created as part of setting up app (cleared with every update)
 
-echo "Updating the system..."
+echo "Updating the system, hang on..."
 apt-get update
 apt-get install -y build-essential  # might not be necessary anymore but I'm not sure
 apt-get install -y nginx  # handles incoming requests, load balances, and passes to uWSGI to be fulfilled
-# potentially add: apt-get install -y git python3 libpython3.7 python3-setuptools
+curl -fsSL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh  # bring in nodejs v14 source
+bash nodesource_setup.sh  # add nodejs v14 source to apt-get
+apt-get install -y nodejs  # install nodejs v14
+apt-get install -y npm  # install npm
+npm install -g turnilo  # install turnilo
 
 echo "Setting up paths..."
 rm -rf ${TMP_PATH}
 mkdir -p ${TMP_PATH}
 mkdir -p ${ETC_PATH}
-mkdir -p ${ETC_PATH}/lt
 mkdir -p ${JAVA_PATH}
+mkdir -p ${DRUID_PATH}
 
 echo "Cloning repositories..."
 git clone --branch ${GIT_BRANCH} ${GIT_CLONE_HTTPS} ${TMP_PATH}/${REPO_LBL}
 
-echo "Downloading language tools, hang on..."
+echo "Downloading Druid, hang on..."
 cd ${TMP_PATH}
-wget -O LanguageTool-stable.zip ${LANGTOOLS_ZIP}
-# if you've already downloaded LanguageTool, you might need to accept all (A) for overwriting
-# NOTE: this currently unzips to LanguageTool-5.6; if that changes, model.service will need to be updated
-unzip LanguageTool-stable.zip -d ${ETC_PATH}/lt
+wget -O "${DRUID_TARNAME}" "${DRUID_APP}"
+shasum -a 512 "${DRUID_TARNAME}" | awk '$1=="${DRUID_EXPECTED_SHASUM}"{exit 1}'
+tar zxvf "${DRUID_TARNAME}" -C "${DRUID_PATH}"
 
 echo "Downloading Java8, hang on..."
 wget -O "${JAVA8_TARNAME}" "${JAVA8_WGET_URL}"
 # check to make sure Java8 file is the expected one
-# Could do the same with the above but I expect LanguageTool to be updated more frequently
 shasum "${JAVA8_TARNAME}" | awk '$1=="${JAVA_EXPECTED_SHASUM}"{exit 1}'
 tar zxvf "${JAVA8_TARNAME}" -C "${JAVA_PATH}"
 # set up Java8 for the system
 sudo update-alternatives --install "/usr/bin/java" "java" "${JAVA_PATH}/${JAVA8_DIRNAME}/bin/java" 1
+
+echo "Downloading data, hang on..."
+cd ${TMP_PATH}
+wget -O data.tsv ${DATA_WGET}
 
 echo "Setting up ownership..."  # makes www-data (how nginx is run) owner + group for all data etc.
 chown -R www-data:www-data ${ETC_PATH}
@@ -61,16 +72,24 @@ chown -R www-data:www-data ${ETC_PATH}
 echo "Copying configuration files..."
 cp ${TMP_PATH}/${REPO_LBL}/model/config/model.nginx ${ETC_PATH}
 cp ${TMP_PATH}/${REPO_LBL}/model/config/model.service ${ETC_PATH}
+cp ${TMP_PATH}/${REPO_LBL}/model/config/druid.service ${ETC_PATH}
+cp ${TMP_PATH}/${REPO_LBL}/model/config/referral-data.json ${ETC_PATH}
 cp ${ETC_PATH}/model.nginx /etc/nginx/sites-available/model
 if [[ -f "/etc/nginx/sites-enabled/model" ]]; then
     unlink /etc/nginx/sites-enabled/model
 fi
 ln -s /etc/nginx/sites-available/model /etc/nginx/sites-enabled/
 cp ${ETC_PATH}/model.service /etc/systemd/system/
+cp ${ETC_PATH}/druid.service /etc/systemd/system/
 
 echo "Enabling and starting services..."
 systemctl enable model.service  # uwsgi starts when server starts up
+systemctl enable druid.service  # uwsgi starts when server starts up
 systemctl daemon-reload  # refresh state
 
 systemctl restart model.service  # start up uwsgi
+systemctl restart druid.service  # start up uwsgi
 systemctl restart nginx  # start up nginx
+
+echo "Loading data into Druid, hang on..."
+"${DRUID_PATH}/${DRUID_DIRNAME}/bin/post-index-task" --file "${ETC_PATH}/referral-data.json" --url http://localhost:8081
