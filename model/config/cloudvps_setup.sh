7785d2efe766e35c568043233397cfcbb8ef0585
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2022-07-02 18:10:42 -0400

    update to druid 0.23 which has logging that shouldn't explode in size

--- model/config/cloudvps_setup.sh
+++ model/config/cloudvps_setup.sh
@@ -11,10 +11,10 @@ GIT_BRANCH='turnilo-druid'
 DATA_WGET='https://analytics.wikimedia.org/published/datasets/one-off/isaacj/referrals/data.tsv'  # data for Turnilo
 
 # Druid database -- this is downloaded and runs the backend database for the search data and turnilo
-DRUID_APP='https://dlcdn.apache.org/druid/0.22.1/apache-druid-0.22.1-bin.tar.gz'
-DRUID_TARNAME='apache-druid-0.22.1-bin.tar.gz'
-DRUID_EXPECTED_SHASUM='716b83e07a76b5c9e0e26dd49028ca088bde81befb070989b41e71f0e8082d11a26601f4ac1e646bf099a4bc7420bdfeb9f7450d6da53d2a6de301e08c3cab0d'
-DRUID_DIRNAME="apache-druid-0.22.1"
+DRUID_APP='https://dlcdn.apache.org/druid/0.23.0/apache-druid-0.23.0-bin.tar.gz'
+DRUID_TARNAME='apache-druid-0.23.0-bin.tar.gz'
+DRUID_EXPECTED_SHASUM='5e7420bdc66247f0309fc0c1eafeff7ba935ed195f3a28c2da4b23a28ff637300b46b1c3d048379ccd570dde164968494ff5d8847e82eaf9b0edb518f8665328'
+DRUID_DIRNAME="apache-druid-0.23.0"
 DRUID_PATH='/var/lib/druid'
 
 # java8 installation -- unfortunately not supported via standard apt-get just yet
