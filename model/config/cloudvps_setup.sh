0fcddd9a564cbb5f42eaf40462da6d4768e7be25
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2026-04-07 14:43:25 -0400

    update to fastapi

--- model/config/cloudvps_setup.sh
+++ model/config/cloudvps_setup.sh
@@ -12,9 +12,7 @@ GIT_BRANCH='gapfinder-deprecation'
 
 # derived paths
 ETC_PATH="/etc/${APP_LBL}"  # app config info, scripts, ML models, etc.
-SRV_PATH="/srv/${APP_LBL}"  # application resources for serving endpoint
 TMP_PATH="/tmp/${APP_LBL}"  # store temporary files created as part of setting up app (cleared with every update)
-LOG_PATH="/var/log/gunicorn"  # application log data
 LIB_PATH="/var/lib/${APP_LBL}"  # where virtualenv will sit
 
 echo "Updating the system..."
@@ -28,14 +26,9 @@ apt-get install -y python3-dev  # necessary for fasttext
 
 echo "Setting up paths..."
 rm -rf ${TMP_PATH}
-rm -rf ${SRV_PATH}
 rm -rf ${ETC_PATH}
-rm -rf ${LOG_PATH}
 rm -rf ${LIB_PATH}
 mkdir -p ${TMP_PATH}
-mkdir -p ${SRV_PATH}/sock
-mkdir -p ${ETC_PATH}/resources
-mkdir -p ${LOG_PATH}
 mkdir -p ${LIB_PATH}
 
 echo "Setting up virtualenv..."
@@ -47,18 +40,15 @@ git clone --branch ${GIT_BRANCH} ${GIT_CLONE_HTTPS} ${TMP_PATH}/${REPO_LBL}
 
 echo "Installing repositories..."
 pip install wheel
-pip install gunicorn
+pip install "fastapi[standard]"
 pip install -r ${TMP_PATH}/${REPO_LBL}/requirements.txt
 
 echo "Setting up ownership..."  # makes www-data (how nginx is run) owner + group for all data etc.
 chown -R www-data:www-data ${ETC_PATH}
-chown -R www-data:www-data ${SRV_PATH}
-chown -R www-data:www-data ${LOG_PATH}
 chown -R www-data:www-data ${LIB_PATH}
 
 echo "Copying configuration files..."
-cp ${TMP_PATH}/${REPO_LBL}/model/config/gunicorn.conf.py ${ETC_PATH}
-cp ${TMP_PATH}/${REPO_LBL}/model/wsgi.py ${ETC_PATH}
+cp ${TMP_PATH}/${REPO_LBL}/model/main.py ${ETC_PATH}
 cp ${TMP_PATH}/${REPO_LBL}/model/config/model.service /etc/systemd/system/
 cp ${TMP_PATH}/${REPO_LBL}/model/config/model.nginx /etc/nginx/sites-available/model
 if [[ -f "/etc/nginx/sites-enabled/model" ]]; then
---
+++ model/main.py
@@ -0,0 +1,32 @@
+import logging
+import os
+
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.middleware.cors import CORSMiddleware
+
+# load in app user-agent or any other app config
+app = FastAPI()
+
+# Enable CORS for API endpoints
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_headers=["*"],
+)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@app.get("/{full_path:path}")
+def deprecated(request: Request, full_path: str):
+    """All endpoints return same deprecation message."""
+    deprecation_message = ("wiki-search-referrals.wmcloud.org has been deprecated. "
+                           "For more details, see: https://wikitech.wikimedia.org/wiki/Data_Platform/Data_Lake/Traffic/referrer_daily/Dashboard. "
+                           "If you have questions, you may reach out to https://meta.wikimedia.org/wiki/User:Isaac_(WMF).")
+    return {"error": deprecation_message}
+
+
+if __name__ == '__main__':
+    app.run()
