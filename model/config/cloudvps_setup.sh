3db38753ba10a07dd97c3df577ab19f3b7dea7d3
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2026-04-07 14:27:10 -0400

    remove flask file that no longer exists

--- model/config/cloudvps_setup.sh
+++ model/config/cloudvps_setup.sh
@@ -59,7 +59,6 @@ chown -R www-data:www-data ${LIB_PATH}
 echo "Copying configuration files..."
 cp ${TMP_PATH}/${REPO_LBL}/model/config/gunicorn.conf.py ${ETC_PATH}
 cp ${TMP_PATH}/${REPO_LBL}/model/wsgi.py ${ETC_PATH}
-cp ${TMP_PATH}/${REPO_LBL}/model/flask_config.yaml ${ETC_PATH}
 cp ${TMP_PATH}/${REPO_LBL}/model/config/model.service /etc/systemd/system/
 cp ${TMP_PATH}/${REPO_LBL}/model/config/model.nginx /etc/nginx/sites-available/model
 if [[ -f "/etc/nginx/sites-enabled/model" ]]; then
