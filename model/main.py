37de154d94b4acabcdd36fdd8d9b3c851ed0adf3
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2026-09-02 14:04:34 -0400

    make actually POST requests instead of GET

--- model/main.py
+++ model/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from sqlitedict import SqliteDict
 
 app = FastAPI()
@@ -11,13 +12,16 @@ app.add_middleware(
 )
 
 SUGGESTIONS_DB = SqliteDict("/etc/api-endpoint/suggestions.db")
-#SUGGESTIONS_DB = SqliteDict("./suggestions.db")
 print(f"There are {len(SUGGESTIONS_DB)} pages in the database.")
 
+class Article(BaseModel):
+    wiki_id: str
+    page_id: int
+
 @app.post('/v1/models/editing-suggestions:predict')
-def get_source_verification_suggestions(wiki_id: str, page_id: int):
+def get_source_verification_suggestions(article:Article):
     """Get similar articles -- optionally filtered by categories, Wikidata, and/or quality."""    
-    return SUGGESTIONS_DB.get(f"{wiki_id}-{page_id}", [])
+    return SUGGESTIONS_DB.get(f"{article.wiki_id}-{article.page_id}", [])
 
 
 if __name__ == "__main__":
--- model/utils.py
+++ model/utils.py
@@ -1,25 +1,31 @@
 import argparse
 import csv
+import traceback
 
 from sqlitedict import SqliteDict
 
-def build_dictionary(input_fn: str, output_fn: str):
+SUGGESTIONS_TO_KEEP = set(["NOT SUPPORTED", "PARTIALLY SUPPORTED"])
+
+def build_dictionary(input_fn: str, output_fn: str, wiki_db: str):
     db = SqliteDict(output_fn)
+    print(f"Starting with {len(db)} pages.")
     data = {}
     suggestions = 0
-    with open(args.input_fn, 'r') as fin:
+    errors = 0
+    skipped = 0
+    with open(input_fn, 'r', encoding='utf-8-sig') as fin:
         csvreader = csv.DictReader(fin)
         for line in csvreader:
             try:
                 verdict = line["verdict"]
-                if verdict == "NOT SUPPORTED":
+                if verdict in SUGGESTIONS_TO_KEEP:
                     revid = int(line['revision_id'])
                     target = line['claim_text']
                     urls = [u for u in line['source_url'].split('\n') if u]
                     comments = line["rationale"]
                     quote = line["source_quote"]
                     page_title = line["page_title"]
-                    wiki_id = line.get('wiki_id', 'enwiki')
+                    wiki_id = line.get('wiki_id', wiki_db)
                     suggestion_id = line['check_id']
                     page_id = int(line['page_id'])
                     key = f"{wiki_id}-{page_id}"
@@ -38,11 +44,15 @@ def build_dictionary(input_fn: str, output_fn: str):
                         "suggestion_id": suggestion_id
                     })
                     suggestions += 1
+                else:
+                    skipped += 1
             except Exception:
+                errors += 1
                 continue
     for k,v in data.items():
         db[k] = v
-    print(f"Committing {suggestions} for {len(data)} pages to {output_fn}.")
+    print(f"Skipped {skipped} suggestions and {errors} had errors.")
+    print(f"Adding {suggestions} suggestions to reach {len(data)} pages in {output_fn}.")
     db.commit()
     db.close()
 
@@ -50,6 +60,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_fn', help="CSV file with suggestions.")
     parser.add_argument('--output_fn', help="Location for SQLite database.")
+    parser.add_argument('--wiki_db', help="Wiki for suggestions -- e.g., `enwiki` for English Wikipedia.")
     args = parser.parse_args()
 
-    build_dictionary(args.input_fn, args.output_fn)
+    build_dictionary(args.input_fn, args.output_fn, args.wiki_db)
